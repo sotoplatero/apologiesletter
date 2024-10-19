f140e55b5f6b21b9e541beaa0fd462e3f5404cbb
Damian Soto <soto.platero@gmail.com>
2024-10-19 10:17:22 -0400

    update

--- .vercel/output/_functions/middleware.mjs
+++
@@ -1,4 +0,0 @@
-import './chunks/astro-designed-error-pages_BHM4SjAq.mjs';
-import './chunks/shared_BnJfrxUh.mjs';
-import './chunks/index_B-h4qkl8.mjs';
-export { onRequest } from './_astro-internal_middleware.mjs';
